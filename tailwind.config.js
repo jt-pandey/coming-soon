348136f587828881cb8a02b7f47498d2d02dd5e0
Jatin Pandey <jatinpandey212@gmail.com>
2023-12-03 00:30:28 +0530

    Coming Soon testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
         groteskb: ["groteskb", "sans-serif"],
         groteskm: ["groteskm", "sans-serif"],
       },
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
     },
   },
   plugins: [],
